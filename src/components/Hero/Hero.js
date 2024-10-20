b8b423aff67e0f56c81e384c0797dfa2618bae78
Mujaffar Husain <93472360+mujhusain@users.noreply.github.com>
2024-10-20 11:24:04 +0000

    revert changes to 1st

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -32,7 +32,7 @@ function Hero() {
               <TypeAnimation
                 cursor={false}
                 sequence={[
-                  'Hi, I\'m Pedro.',
+                  'Hi, I\'m Mujaffar.',
                   () => setShowSubtitle(true)
                 ]}
                 speed={{ type: "keyStrokeDelayInMs", value: 150 }}
